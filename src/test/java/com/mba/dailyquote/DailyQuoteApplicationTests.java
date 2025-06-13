4d3f5bbeabc09553a08d63384372bd16b840b3c6
Burak Altun <52549565+mburakaltun@users.noreply.github.com>
2025-06-14 02:00:45 +0300

    initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ src/test/java/com/mba/dailyquote/DailyQuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mba.dailyquote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyQuoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
