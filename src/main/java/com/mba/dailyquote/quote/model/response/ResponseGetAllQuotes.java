31bcf2206a4f1cbd3c25515e4cc826aa03d85270
Burak Altun <52549565+mburakaltun@users.noreply.github.com>
2025-06-16 21:11:24 +0300

    remove unused Lombok @Builder annotation from ResponseGetAllQuotes

--- src/main/java/com/mba/dailyquote/quote/model/response/ResponseGetAllQuotes.java
+++ src/main/java/com/mba/dailyquote/quote/model/response/ResponseGetAllQuotes.java
@@ -2,7 +2,6 @@ package com.mba.dailyquote.quote.model.response;
 
 import com.mba.dailyquote.common.model.response.PageableResponse;
 import com.mba.dailyquote.quote.model.dto.QuoteDto;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
