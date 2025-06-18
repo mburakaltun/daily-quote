e0676e60c6f5604fa52fd155c5abffa89dfab65f
Burak Altun <52549565+mburakaltun@users.noreply.github.com>
2025-06-18 19:28:54 +0300

    add Redis cache manager and enable caching in the application

--- src/main/java/com/mba/dailyquote/DailyQuoteApplication.java
+++ src/main/java/com/mba/dailyquote/DailyQuoteApplication.java
@@ -2,10 +2,12 @@ package com.mba.dailyquote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class DailyQuoteApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/mba/dailyquote/common/config/CacheConfig.java
@@ -0,0 +1,21 @@
+package com.mba.dailyquote.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig())
+                .build();
+    }
+}
+
+
